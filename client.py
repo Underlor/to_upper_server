98d9fd326aef7beef6c3f9652b5face90ea6e840
Underlor <Cfyz_74@mail.ru>
2017-09-20 19:50:51 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 import socket
 
-data_len = 12;
+data_len = 12
+url = "127.0.0.1"
+port = 8081
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('mta-sa.ru', 8081))
+s.connect((url, port))
 str = (b'h'*data_len) + b'\x00'
 s.send(str)
 
 upper = s.recv(len(str)-1)
 print(upper)
 s.close()
+
